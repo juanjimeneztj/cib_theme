c3ab7c98401f796b9658019ff817ebc409c94c47
Juan Jimenez <juanjimeneztj@gmail.com>
2020-10-23 10:12:30 -0700

    updated footer logo and image of the bottom section

--- footer.php
+++ footer.php
@@ -7,8 +7,8 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col col-sm-4 col-md-3 col-lg-3 col-lx-2">
-                        <div class="logo-footer"><?=get_custom_logo()?></div>
+                    <div class="col col-sm-4 col-md-3 col-lg-2 col-lx-2">
+                        <div class="logo-footer"><img src="<?=get_template_directory_uri()?>/images/Classified_Intelligence_Brief_Footer.png" alt=""></div>
                         <div class="navigation-footer">
                             <?php
                                 wp_nav_menu( array(
@@ -18,7 +18,7 @@
                             ?>
                         </div>
                     </div>
-                    <div class="col-12 col-sm-12 col-md-12 col-lg-9 col-lx-10">
+                    <div class="col-12 col-sm-12 col-md-12 col-lg-10 col-lx-10">
                         <p>HIGH RISK WARNING</p>
                         <p class="text-justify">Trading foreign exchange, stocks, options, or futures on margin carries a high level of risk, and may not be suitable for all investors. Before deciding to trade, you should carefully consider your objectives, financial situation, needs and level of experience. Classified Intelligence Brief provides general advice that does not take into account your objectives, financial situation or needs. The content of this website must not be construed as personal advice. The possibility exists that you could sustain a loss in excess of your deposited funds and therefore, you should not speculate with capital that you cannot afford to lose. You should be aware of all the risks associated with trading on margin.You should seek advice from an independent financial advisor. Past performance is not necessarily indicative of future success.</p>
                         <p>101 Marketside Ave. Suite 404 PMB 318 Ponte Vedra, FL 32081  |  (321) 325-1551</p>
